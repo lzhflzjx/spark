4b1bcdc496f7248bdfd04e96724c89f78cfca5c5
itw_wangshuo06 <itw_wangshuo06@tkmail.com>
2024-09-06 17:57:00 +0800

    feat:富文本编辑器

--- src/components/WorkHistory.vue
+++ src/components/WorkHistory.vue
@@ -7,6 +7,7 @@
                     Department: Technical Department
                     Position: Web Front-end Engineer
                     Responsible Platforms: APP H5, WeChat H5/Mini Program, China Merchants Bank Mini Program, wap site
+                    <el-divider />
                     Duties:
                     1. Responsible for evaluating all known product requirements with the product team, assessing and
                     determining development content and timelines.
@@ -18,25 +19,45 @@
                     4. Maintain good communication with product and backend developers, quickly understand and digest the
                     needs of all parties, and implement them into specific development work.
                     5. Maintain and iterate the company's react/jq/mini program and other ERP/PM/OMS management projects,
-                    ensuring smooth operation online.</p>
+                    ensuring smooth operation online.
+                </p>
             </el-card>
         </el-timeline-item>
         <el-timeline-item timestamp="2016/6 - 2020/12" placement="top">
             <el-card>
                 <h4>Company Name: Beijing Anxun Weiye Co., Ltd.</h4>
                 <p>
-                    Department: Technical Department
-                    Position: Web Front-end Engineer
-                    Duties:
-                    1. Responsible for evaluating all known product requirements with the product team, assessing and
-                    determining development content and timelines.
-                    2. Responsible for building evaluated projects, providing reasonable front-end architecture,
-                    implementing project requirements with reasonable technical solutions to ensure project iterability and
-                    scalability.
-                    3. Responsible for the requirements of related products and the implementation of front-end programs,
-                    improving and optimizing development tools, processes, and frameworks.
-                    4. Maintain good communication with product and backend developers, quickly understand and digest the
-                    needs of all parties, and implement them into specific development work</p>
+                <p class="ne-p" style="margin-top: 0px; margin-bottom: 0px; padding: 0px; min-height: 24px;">
+                    <span class="ne-text">Department: Technical Department</span>
+                </p>
+                <p class="ne-p" style="margin-top: 0px; margin-bottom: 0px; padding: 0px; min-height: 24px;">
+                    <span class="ne-text">Position: Web Front-end Engineer</span>
+                </p>
+                <p class="ne-p" style="margin-top: 0px; margin-bottom: 0px; padding: 0px; min-height: 24px;">
+                    <span class="ne-text">Duties:</span>
+                </p>
+                <p class="ne-p" style="margin-top: 0px; margin-bottom: 0px; padding: 0px; min-height: 24px;">
+                    <span class="ne-text">1. Responsible for evaluating all known product requirements with the product
+                        team, assessing and determining development content and timelines.</span>
+                </p>
+                <p class="ne-p" style="margin-top: 0px; margin-bottom: 0px; padding: 0px; min-height: 24px;">
+                    <span class="ne-text">2. Responsible for building evaluated projects, providing reasonable front-end
+                        architecture, implementing project requirements with reasonable technical solutions to ensure
+                        project iterability and scalability.</span>
+                </p>
+                <p class="ne-p" style="margin-top: 0px; margin-bottom: 0px; padding: 0px; min-height: 24px;">
+                    <span class="ne-text">3. Responsible for the requirements of related products and the implementation of
+                        front-end programs, improving and optimizing development tools, processes, and frameworks.</span>
+                </p>
+                <p class="ne-p" style="margin-top: 0px; margin-bottom: 0px; padding: 0px; min-height: 24px;">
+                    <span class="ne-text">4. Maintain good communication with product and backend developers, quickly
+                        understand and digest the needs of all parties, and implement them into specific development
+                        work</span>
+                </p>
+                <p>
+                    <br />
+                </p>
+                </p>
             </el-card>
         </el-timeline-item>
     </el-timeline>
